9174d40fd13814dbd7403a6b3d39d7c71813d99e
PogromistDev <muradazizov@outlook.com>
2022-12-06 01:36:27 +0400

    QR Code
    Showing QR Code of <ip-address:port> for easier access to web ui
    Help text update

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
-var mime = require("mime-types");
+const fs = require("fs");
+const mime = require("mime-types");
 
-var path = require("path");
-var ip = require("ip");
+const path = require("path");
+const ip = require("ip");
 
-var http = require("http");
-var { spawnSync } = require("child_process");
+const http = require("http");
+const { spawnSync } = require("child_process");
+
+const QRCode = require('qrcode');
 
 const help = require("./help.txt");
 
 // host:port
 
-var host = ip.address();
-var localhost = "127.0.0.1";
+var host = "";
+var externalAddress = false;
+
+const localhost = "127.0.0.1";
 var port = 8080;
+
+var showQR = false;
 
 process.chdir(__dirname);
 
@@ -23,17 +29,37 @@ process.chdir(__dirname);
 
 for (let [index, arg] of process.argv.entries()) {
 
-	if (arg == "-p") {
-		port = parseInt(process.argv[index + 1]);
+
+	if (["-n", "--name", "--in", "--interface-name"].includes(arg)) {
+		try {
+			host = ip.address(process.argv[index + 1]);
+			externalAddress = true;
+		}
+		catch (e) {
+			externalAddress = false;
+		}
+		continue;
 	}
 
-	if (arg == "-h" || arg == "-a") {
+	if (["-p", "--port"].includes(arg)) {
+		port = parseInt(process.argv[index + 1]);
+		continue;
+	}
+
+	if (["-a", "-h", "--ip", "--address", "--host", "--ip-address"].includes(arg)) {
 		host = process.argv[index + 1];
+		externalAddress = true;
+		continue;
 	}
 
 	if (["--help", "-?", "/?", "/help", "/h"].includes(arg)) {
 		console.log(help);
 		process.exit(0);
+	}
+
+	if (["--show-qr-code", "--qr"].includes(arg)) {
+		showQR = true;
+		continue;
 	}
 }
 
@@ -46,7 +72,16 @@ var server = http.createServer(requestHandler);
 try {
 	server.listen(port, '0.0.0.0', 0, () => {
 		console.log(`Running on ${localhost}:${port}`);
-		console.log(`Running on ${host}:${port}`);
+		if (externalAddress) {
+			console.log(`Running on ${host}:${port}`);
+			if (showQR) {
+
+				QRCode.toString(`http://${host}:${port}"`, { type: 'terminal' }, (err, url) => {
+					if (!err) console.log("\n" + url);
+				});
+
+			}
+		}
 	});
 
 }
@@ -66,7 +101,7 @@ server.on("error", (e) => {
 
 function executeScreenshotter(args, req, resp) {
 	var result = spawnSync("app/devrantscreenshot", args, {});
-	
+
 	try {
 		var buffer = fs.readFileSync("shot.png");
 	}
@@ -89,7 +124,7 @@ function requestHandler(req, resp) {
 	if (req.method == "GET") {
 
 		// screenshot
-	
+
 		if (req.url == "/screenshot") {
 
 			console.log(`Cropped screenshot [${req.connection.remoteAddress}]`);
@@ -102,22 +137,22 @@ function requestHandler(req, resp) {
 			executeScreenshotter(["--full-screen"], req, resp);
 			return;
 		}
-	
+
 		// root document
-	
+
 		filename = req.url;
 
 		if (filename == "/") filename = "/web/index.html";
-		
+
 		// file request handling
-	
+
 		fs.readFile(filename.substring(1), (err, data) => {
 			if (err) {
 				resp.statusCode = 404;
 				resp.end();
 				return;
 			}
-			
+
 			resp.statusCode = 200;
 			resp.setHeader("Content-Type", mime.lookup(path.extname(filename)) || "application/octet-stream");
 			resp.write(data);
